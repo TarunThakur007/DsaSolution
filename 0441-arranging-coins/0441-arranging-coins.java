0bb096d062e750423afe2faf6f0bd8bbb26f6413
Tarun Pratap Singh <tarunsom100@gmail.com>
2026-09-16 21:19:41 +0530

    Time: 1 ms (100%), Space: 42.3 MB (97.87%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,10 +1,28 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int row=0;
-        while(n>=row+1){
-            row++;
-            n-=row;
+    //     int row=0;
+    //     while(n>=row+1){
+    //         row++;
+    //         n-=row;
+    //     }
+    //     return row;
+    // }
+
+        long left = 1;
+        long right = n;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long coins = mid * (mid + 1) / 2;
+            if (coins == n) {
+                return (int) mid;
+            }
+            else if (coins < n) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        return row;
+        return (int) right;
     }
 }
