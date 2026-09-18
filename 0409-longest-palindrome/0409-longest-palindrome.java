416e0fe455462516dcf66ceb7a5f28de91384b5b
Tarun Pratap Singh <tarunsom100@gmail.com>
2026-09-18 11:47:15 +0530

    Time: 1 ms (99.86%), Space: 43.2 MB (39.84%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] arr = new int[128];
+        for(char ch : s.toCharArray()){
+            arr[ch]++;
+        }
+        int a=0;
+        boolean odd = false;
+        for(int count : arr){
+            a+=(count/2)*2;
+            if(count%2==1){
+                odd = true;
+            }
+        }
+        if(odd){
+            a+=1;
+        }
+        return a;
+    }
+}
